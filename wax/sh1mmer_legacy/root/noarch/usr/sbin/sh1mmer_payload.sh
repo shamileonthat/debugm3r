df69ffb729920b7b9ec71390b3746828280b2262
shamileonthat <shamileonthat2@gmail.com>
2026-01-01 08:35:17 -0700

    Update sh1mmer_payload.sh
    ill probably have to add some more payloads tbh but this is good for right now

--- wax/sh1mmer_legacy/root/noarch/usr/sbin/sh1mmer_payload.sh
+++ wax/sh1mmer_legacy/root/noarch/usr/sbin/sh1mmer_payload.sh
@@ -59,18 +59,8 @@ run_task() {
 
 mapname() {
 	case "$(basename "$1")" in
-		'autoupdate.sh') echo -n "Fetch updated payloads. REQUIRES INTERNET CONNECTION" ;;
-		'br0ker.sh') echo -n "Br0ker (unenrollment up to r132/kernver 5)";;
-		'caliginosity.sh') echo -n "Revert all changes made by sh1mmer (reenroll + more)" ;;
 		'crap.sh') echo -n "CRAP - ChromeOS Automated Partitioning" ;;
-		'cryptosmite.sh') echo -n "Cryptosmite (unenrollment up to r119, by writable)" ;;
-		'defog.sh') echo -n "Set GBB flags to allow devmode and unenrollment on r112-113. WRITE PROTECTION MUST BE DISABLED" ;;
-		'icarus.sh') echo -n "Icarus (unenrollment up to r129, by writable)";;
-		'movie.sh') echo -n "HAHA WINDOWS SUX BUT THE MOVIE" ;;
 		'mrchromebox.sh') echo -n "MrChromebox firmware-util.sh" ;;
-		'quicksilver.sh') echo -n "Quicksilver (unenrollment up to r142, by emerwyi)";;
-		'reset-kern-rollback.sh') echo -n "Reset kernel rollback version" ;;
-		'troll.sh') echo -n "hahah wouldn't it be realllly funny if you ran this payload trust me nothing bad will happen" ;;
 		'weston.sh') echo -n "Launch the weston Desktop Environment. REQUIRES A DEVSHIM" ;;
 		'wifi.sh') echo -n "Connect to wifi" ;;
 		'wp-disable.sh') echo -n "WP disable loop (for pencil method)" ;;
